1b6b72b23f54ce813f79cdda54cbb2bc6cae535c
mtrs163 <153596133+mtrs163@users.noreply.github.com>
2025-08-18 12:03:11 +0700

    fix publishing hopefully (#138)

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -15,7 +15,7 @@ RELEASE_DIR = "release"
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "https://5.189.196.198/"
+ROBUST_CDN_URL = "http://5.189.196.198:27690/"
 FORK_ID = "collardgreenssuperserver"
 
 def main():
