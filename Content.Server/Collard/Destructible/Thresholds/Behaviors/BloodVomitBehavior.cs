f3698fe12f07ff266786303141c6704ec9c8df59
mtrs163 <153596133+mtrs163@users.noreply.github.com>
2025-09-02 21:58:51 +0700

    Add blood vomit (#154)

---
+++ Content.Server/Collard/Destructible/Thresholds/Behaviors/BloodVomitBehavior.cs
@@ -0,0 +1,16 @@
+using Content.Server.Medical;
+using Content.Server.Destructible.Thresholds.Behaviors;
+using Content.Server.Destructible;
+using Content.Server.Body.Systems;
+
+namespace Content.Server.Collard.Destructible.Thresholds.Behaviors;
+
+[DataDefinition]
+public sealed partial class BloodVomitBehavior : IThresholdBehavior
+{
+    public void Execute(EntityUid uid, DestructibleSystem system, EntityUid? cause = null)
+    {
+        system.EntityManager.System<VomitSystem>().Vomit(uid);
+        system.EntityManager.System<BloodstreamSystem>().TryModifyBloodLevel(uid, -50);
+    }
+}
